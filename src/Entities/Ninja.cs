9d39b9ab5bd09a5aa626c86c697df53fcc81d262
Yanna Pecanha <yannarp@hotmail.com>
2022-03-18 17:55:53 -0300

    Initial commit

---
+++ src/Entities/Ninja.cs
@@ -0,0 +1,18 @@
+namespace abstraindojogo.src.Entities
+{
+    public class Ninja : Hero
+    {
+
+        public Ninja(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Atacou com um golpe ";
+        }
+    }
+}
---
+++ src/Entities/WhiteWizard.cs
@@ -0,0 +1,30 @@
+namespace abstraindojogo.src.Entities
+{
+    public class WhiteWizard : Hero
+    {
+        public WhiteWizard(string Name, int Level, string HeroType) : base(Name, Level, HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+
+        public override string Attack()
+        {
+            return this.Name + " Lançou magia ";
+        }
+
+        public string Attack(int Bonus)
+        {
+
+            if (Bonus > 6)
+            {
+                return this.Name + " Lançou Super Magia com Bonus de " + Bonus;
+            }
+            else
+            {
+                return this.Name + " Lançou Magia com Bonus de " + Bonus;
+            }
+        }
+    }
+}
